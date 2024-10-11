00a8ca98a738e8dcf8a631d333762246759c93dc
LittleDemonh <cor.eulitz@gmail.com>
2024-10-11 10:28:10 +0200

    updated def printHand

--- src/main/scala/Kitty_cards.scala
+++ src/main/scala/Kitty_cards.scala
@@ -1,63 +1,65 @@
-object KittyCardGame {
-  def main(args: Array[String]): Unit = {
-    println("Welcome to the Kitty Card Game!")
-    // Create an instance of the Kitty_cards class to run its logic
-    new Kitty_cards()
-  }
-
-  // Define the Kitty_cards class outside of the main method
-  class Kitty_cards {
-    // Cards in two hands
-    val hand1 = List("S", "6") // First hand (left)
-    val hand2 = List("I", "9") // Second hand (right)
-
-    // Call the methods to print the hands and the cats
-    printHand(hand1)
-    printCats()
-    printHand(hand2)
-  //
-    //
-    // Function to print a hand of cards
-    def printHand(hand: List[String]): Unit = {
-      // Print the top border of the cards
-      for (_ <- hand) {
-        print("  +---+")
-      }
-      println()
-
-      // Print the card values
-      for (card <- hand) {
-        print(s"  | $card |")
-      }
-      println()
-
-      // Print the bottom border of the cards
-      for (_ <- hand) {
-        print("  +---+")
-      }
-      println()
-      println() // Blank line after the hand
+object Kitty_cards {
+    def main(args: Array[String]): Unit = {
+        println("Welcome to the Kitty Card Game!")
+        // Create an instance of the Kitty_cards class to run its logic
+        new Kitty_cards()
     }
-//setting up corina branch
-    // Function to print the cats
-    def printCats(): Unit = {
-      println("________________________________________")
-      println("|            |            |            |")
-      println("|            |   ≽^•⩊•^≼  |            |")
-      println("|            |            |            |")
-      println("__________________________________________")
-      println("|            |            |            |")
-      println("|            |  ≽^•⩊•^≼   |  ≽^•⩊•^≼   |" )
-      println("|            |            |            |")
-      println("__________________________________________")
-      println("|            |            |             |")
-      println("|  ≽^•⩊•^≼   |  ≽^•⩊•^≼  |             |" )
-      println("|            |            |             |")
-      println("________________________________________")
-      //kittys
-//setting up shahed lol
-      println()
-      //setting up shahed branch
+
+    // Define the Kitty_cards class outside of the main method
+    private class Kitty_cards {
+        // Cards in two hands
+        private val hand1 = List("S", "6") // First hand (left)
+        private val hand2 = List("I", "9") // Second hand (right)
+
+        // Call the methods to print the hands and the cats
+        printHand(hand1)
+        printCats()
+        printHand(hand2)
+
+        //
+        //
+        // Function to print a hand of cards
+        private def printHand(hand: List[String]): Unit = {
+            // Print the top border of the cards
+            for (_ <- hand) {
+                print("  +---+")
+            }
+            println()
+
+            // Print the card values
+            for (card <- hand) {
+                print(s"  | $card |")
+            }
+            println()
+
+            // Print the bottom border of the cards
+            for (_ <- hand) {
+                print("  +---+")
+            }
+            println()
+            println() // Blank line after the hand
+        }
+
+        //setting up corina branch
+        // Function to print the cats
+        private def printCats(): Unit = {
+            println("________________________________________")
+            println("|            |            |            |")
+            println("|            |   ≽^•⩊•^≼  |            |")
+            println("|            |            |            |")
+            println("__________________________________________")
+            println("|            |            |            |")
+            println("|            |  ≽^•⩊•^≼   |  ≽^•⩊•^≼   |")
+            println("|            |            |            |")
+            println("__________________________________________")
+            println("|            |            |             |")
+            println("|  ≽^•⩊•^≼   |  ≽^•⩊•^≼  |             |")
+            println("|            |            |             |")
+            println("________________________________________")
+            //kittys
+            //setting up shahed lol
+            println()
+            //setting up shahed branch
+        }
     }
-  }
 }
---
+++ src/main/scala/Test.scala
@@ -0,0 +1,54 @@
+object Test {
+
+    def main(args: Array[String]): Unit = {
+
+        val eol = sys.props("line.separator") // End of line character
+
+        def bar(cellWidth: Int = 15, cellNum: Int = 3): String =
+            ("+" + "-" * cellWidth) * cellNum + "+" + eol
+
+        def cells(cellWidth: Int = 15, cellNum: Int = 3): String =
+            (("|" + " " * cellWidth) * cellNum + "|" + eol) * 4
+
+        def mesh(cellWidth: Int = 15, cellNum: Int = 3): String =
+            (bar(cellWidth, cellNum) + cells(cellWidth, cellNum)) * cellNum + bar(cellWidth, cellNum)
+
+        def printHeading(): Unit =
+            println(Console.MAGENTA + "Welcome to the Kitty Card Game!" + Console.RESET + eol)
+
+        def card(color: String, value: Int): String = {
+            val brightWhite = "\u001b[97m"
+            val cardTop = color + " ∧,,,∧"
+            val cardMiddle = s"( ̳• · •̳) " + brightWhite + value
+            val cardBottom = color + "/    づ♡" + Console.RESET
+            // s string interpolator, multiline string
+            val cardContent = s"$cardTop$eol$cardMiddle$eol$cardBottom$eol"
+            cardContent
+        }
+
+        // Cards in two hands
+        val hand1 = List("S", "6") // First hand (left)
+        val hand2 = List("I", "9") // Second hand (right)
+
+        // Function to print a hand of cards
+        def printHand(hand: List[String]): Unit = {
+            val topBorder = hand.map(_ => "  +---+").mkString + eol
+            val cardValues = hand.map(card => s"  | $card |").mkString + eol
+            val bottomBorder = hand.map(_ => "  +---+").mkString + eol
+            print(topBorder + cardValues + bottomBorder + eol)
+        }
+
+        printHeading()
+
+        printHand(hand1)
+        println(mesh())
+        printHand(hand2)
+
+        println(card(Console.BLUE, 3))
+        println(card(Console.RED, 10))
+        println(card(Console.GREEN, 8))
+        print(card(Console.YELLOW, 1))
+
+
+    }
+}
