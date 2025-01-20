c0514953cb0675bd37f4e191b6eebef02ee78442
LittleDemonh <cor.eulitz@gmail.com>
2025-01-21 00:00:46 +0100

    final test change

--- src/test/scala/controller/GameControllerSpec.scala
+++ src/test/scala/controller/GameControllerSpec.scala
@@ -186,8 +186,8 @@ class GameControllerSpec extends AnyWordSpec with Matchers with MockitoSugar {
 
             reset(observer)
             controller.handleCommand("redo")
-            verify(observer, times(2)).update(any[RedoEvent])
-            verify(observer, times(2)).update(any[ShowCardsForPlayer])
+            verify(observer, times(6)).update(any[RedoEvent])
+            verify(observer, times(6)).update(any[ShowCardsForPlayer])
         }
 
         "handle errors correctly when loading the game" in {
