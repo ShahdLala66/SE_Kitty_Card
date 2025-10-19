6c84db27dab53c19ea3572b75bddf052c5f64e42
shahdlala66 <shahd.lala66@gmail.com>
2025-10-19 17:48:07 +0200

    feat: websockets

--- src/main/scala/main_/Main.scala
+++ src/main/scala/main_/Main.scala
@@ -1,16 +1,19 @@
 package main_
 
-import aview.Tui
-import aview.gui.Gui
+// import aview.Tui  // Disabled for web/WebSocket mode
+// import aview.gui.Gui  // Disabled for Kubernetes deployment
 import com.google.inject.Guice
 import controller.GameControllerInterface
 
 object Main extends App {
     private val injector = Guice.createInjector(new XmlModule)
     val controller = injector.getInstance(classOf[GameControllerInterface])
-    val tui = new Tui(controller)
-    val gui = new Gui(controller)
+    
+    // TUI and GUI disabled for web/Kubernetes deployment - using WebSocket instead
+    // val tui = new Tui(controller)
+    // val gui = new Gui(controller)
 
-    controller.startGame()
+    // Don't start the game automatically - wait for WebSocket connection
+    // controller.startGame()
    
 }
